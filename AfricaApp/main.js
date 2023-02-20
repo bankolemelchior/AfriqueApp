358181c1eafcdfa227e98fbb5bd943ca9c81ded6
OMOTOSHO <bankolemelchior@gmail.com>
2023-02-20 18:21:50 +0100

    adding newest features 20-02-2023

--- AfricaApp/main.js
+++ AfricaApp/main.js
@@ -2,36 +2,35 @@ import {country, flags} from './country.js';
 
 
 
-
-// const [AFS, Benin, ...rest] = country;
-
-// function forDest({Name, phoneCode, code, Currency, Area, capitalCity}) {
-//     console.log('La capital de ' + Name + ' est ' + capitalCity);
-// }
-// forDest(AFS);
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const pays = document.getElementById('pays');
     const gameScore = document.querySelector('.score');
+    const quizDiv = document.querySelector('.quiz');
+    const quizTitle = document.querySelector('.quizTitle');
     
     const allAnswers = document.getElementById('allAnswers');
     const reloadBtn = document.getElementById('reload');
+    const nextBtn = document.querySelector('.forNext');
+    const endGame = document.querySelector('.endGame');
+    const startAgain = document.querySelector('.startAgain');
     
     let score = 0;
     let numberOfQuestion = 0;
+    let questionNumb;
     
     function loadQuiz() {
         let index1 = Math.round(Math.random()*53);
         let index2 = Math.round(Math.random()*53);
         let index3 = Math.round(Math.random()*53);
         let possibleAnswers = [];
+
         
         possibleAnswers.push(country[index1].capitalCity, country[index2].capitalCity, country[index3].capitalCity);
         let sortAnswers = possibleAnswers.sort();
         console.log(possibleAnswers);
+        console.log(sortAnswers);
         
-        pays.textContent = country[index1].Name;
+        quizTitle.textContent = `Quelle est la capitale de ce pays: ${country[index1].Name}`;
         gameScore.textContent = `Score: ${score}/${numberOfQuestion}`
 
         
@@ -61,8 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 answers.forEach(el => {
                     el.addEventListener('input', () => {
-                        if(el.checked) {
-                
+                        if(el.checked) {                
 
                             //To make desabled the inputs radio so as to not check again
                             answers.forEach(input => {
@@ -74,12 +72,11 @@ document.addEventListener('DOMContentLoaded', function() {
                                 score++;
     
                                 el.nextSibling.style.color = "green";
-                                // score++;
 
                                 //Create a span to tell this was the correct answer
                                 let showAnswer = document.createElement('span');
                                 showAnswer.setAttribute('class', 'showAnswer');
-                                showAnswer.textContent =`Bien jouer, c'est la bonne reponse! `;
+                                showAnswer.textContent =`✅ Bien jouer, c'est la bonne reponse! `;
                                 allAnswers.append(showAnswer);
 
                             }else {
@@ -87,13 +84,12 @@ document.addEventListener('DOMContentLoaded', function() {
                                 //Create a span to display the correct answer after checking the radio input
                                 let showAnswer = document.createElement('span');
                                 showAnswer.setAttribute('class', 'showAnswer');
-                                showAnswer.textContent =`Mauvaise reponse, la bonne reponse est ${country[index1].capitalCity}`;
+                                showAnswer.textContent =`❌ Mauvaise reponse, la bonne reponse est ${country[index1].capitalCity}`;
                                 allAnswers.append(showAnswer)
         
                             }
 
                             gameScore.textContent = `Score: ${score}/${numberOfQuestion}`
-                            // console.log(score);
 
                         }
     
@@ -154,7 +150,7 @@ document.addEventListener('DOMContentLoaded', function() {
                                 //Create a span to tell this was the correct answer
                                 let showAnswer = document.createElement('span');
                                 showAnswer.setAttribute('class', 'showAnswer');
-                                showAnswer.textContent =`Bien jouer, c'est la bonne reponse! `;
+                                showAnswer.textContent =`✅ Bien jouer, c'est la bonne reponse! `;
                                 allAnswers.append(showAnswer);
 
                                 
@@ -164,7 +160,7 @@ document.addEventListener('DOMContentLoaded', function() {
                                 //Create a span to display the correct answer after checking the radio input
                                 let showAnswer = document.createElement('span');
                                 showAnswer.setAttribute('class', 'showAnswer');
-                                showAnswer.textContent =`Mauvaise reponse, la bonne reponse est ${country[index1].capitalCity}`;
+                                showAnswer.textContent =`❌ Mauvaise reponse, la bonne reponse est ${country[index1].capitalCity}`;
                                 allAnswers.append(showAnswer)
         
                             }
@@ -180,18 +176,63 @@ document.addEventListener('DOMContentLoaded', function() {
 
         }
 
+
         reloadBtn.addEventListener('click', () => {
             loadQuiz();
             numberOfQuestion++;
-            console.log(numberOfQuestion);
-
-            if(reloadBtn.textContent === 'Commencer') {
-                reloadBtn.textContent = 'Continuer';
-            }
+            questionNumb = window.prompt("Entrez le nombre de question");
+            reloadBtn.style.display = "none";
+            nextBtn.style.display = "block";
+            startAgain.style.display = "block";
+            
         });
+        
+        nextBtn.addEventListener('click', () => {
+            loadQuiz();
+            numberOfQuestion++;
+
+            if((numberOfQuestion) == parseInt(questionNumb)) {
+                nextBtn.textContent = "Finir";
+            }
+            
+            if((numberOfQuestion - 1) == parseInt(questionNumb)) {
+
+                startAgain.style.display = "block";
+                nextBtn.style.display = "none";
+
+
+                if(score == parseInt(questionNumb)) {
+
+                    endGame.innerHTML = `<p>Score: ${score}/${questionNumb}</p>
+                                        <p>Tu es un 10 💥</p>
+                                        <span class="spanEndGame">⭐⭐⭐</span>
+
+                    
+                    `
+                }else {
+                    
+                    endGame.innerHTML = `Score: ${score}/${questionNumb}
+                                        <p>Peut faire mieux 🤨</p>
+                                        <span class="spanEndGame">⭐⭐</span>`
+
+                }
+
+                quizDiv.style.display = "none";
+                endGame.style.display = "block";
+
+
+            }
+
+        })
+
+        startAgain.addEventListener('click', () => {
+            window.location.reload();
+        })
 
     
     })
+
+    //Seaching country
  
     let forSearching = document.querySelector('.forSearching');
     let searchForm = document.querySelector('#searchForm');    
@@ -199,7 +240,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     forSearching.addEventListener('input', (e) => {
         e.preventDefault();
-        let str = e.target.value;
+        let str = (e.target.value).toLowerCase();
+        str = str.replace(str.charAt(0), str.charAt(0).toUpperCase())
+        console.log(str);
         
         let forData = fetch('data.json')
         .then(resp => {
@@ -212,6 +255,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 //To get in array all objet which match the if statement
                 let arraySuggestion = data.filter(element => {
+
                 if(str && (element.Name).substr(0, str.length) === str) {
                     return element;
                 }
@@ -227,9 +271,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 //To iterate through the array which containe the suggesions and create html tags
                 arraySuggestion.forEach(element => {
                 
-                    let sug = document.createElement('p');
+                    let sug = document.createElement('span');
                     sug.setAttribute('class','forSuggestion');
-                    sug.innerHTML = element.Name;
+                    sug.innerHTML = `${element.Name},`;
                     getSuggestion.append(sug);
                     search.append(getSuggestion);
 
@@ -240,11 +284,47 @@ document.addEventListener('DOMContentLoaded', function() {
                 //And iterate through 
                 allSugg.forEach(elmt => {
                     elmt.addEventListener('click', () => {
+                        console.log(((elmt.textContent).split(","))[0]);
+
                         data.forEach(element => {
 
-                            if(elmt.textContent === element.Name) {
+                            if(((elmt.textContent).split(","))[0] === element.Name) {
                                 forSearching.value = element.Name
-                                console.log(element);
+
+                                document.querySelector(".displayCountry").style.border = 5 + "px " + "solid " + "green";
+                                document.querySelector(".displayCountry").style.paddingInline = 35 + "px";
+                                document.querySelector(".displayCountry").innerHTML = 
+                                
+                                `                                
+                                <div>
+                                    <h2>${element.Name}</h2>
+                                    <div class="phone_code">
+                                        <h4>Indicatif téléphonique : </h4>
+                                        <p>${element.phoneCode}</p>
+                                    </div>
+
+                                    <div class="currency">
+                                        <h4>Monnaie : 💲</h4>
+                                        <p>${element.Currency}</p>
+                                    </div>
+
+                                    <div class="country_code">
+                                        <h4>Code monnaie : </h4>
+                                        <p>${element.code}</p>
+                                    </div>
+
+                                    <div class="capital_city">
+                                        <h4>Capitale : 🗽</h4>
+                                        <p>${element.capitalCity}</p>
+                                    </div>
+
+                                    <div class="area">
+                                        <h4>Zone géographique: 🌍</h4>
+                                        <p>${element.Area}</p>
+                                    </div>
+                                </div>                
+                                
+                                `
                             }
 
                         });
